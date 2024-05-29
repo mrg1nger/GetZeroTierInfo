c1da57d3851fa21b5d4ea6b733a1f8ec2491b915
Mr.Ginger <103669103+mrg1nger@users.noreply.github.com>
2024-05-29 11:59:10 -0600

    Add files via upload

---
+++ ZerotierInfoAPI.ps1
@@ -0,0 +1,17 @@
+﻿$bearer_token = "Bearer <token here>"
+$network_id = "<networkid here>"
+while($true){
+CLEAR
+$network_members = Invoke-RestMethod -Headers @{Authorization = $bearer_token} -Uri "https://api.zerotier.com/api/v1/network/$network_id/member" -Method GET -ContentType 'application/json'
+foreach($member in $network_members){
+	$online_check = $member.clock - $member.lastSeen
+	if ($online_check -lt 100000) {
+		$is_online = "Online"
+	} else {
+		$is_online = "*OFFLINE*"
+	}
+	Write-Host $member.name "-" $member.config.ipAssignments "- Authorized:" $member.config.authorized "- Time Offline:" $online_check "-" $is_online
+}
+Start-sleep -Seconds "900"
+CLEAR
+}
